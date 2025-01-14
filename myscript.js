06d0afb5071ddcbd9d6eab70314166937ad94ffa
Narendran <175111990+Twenty625@users.noreply.github.com>
2025-01-15 03:45:58 +0530

    Write the logic to play a single round.

--- myscript.js
+++ myscript.js
@@ -20,3 +20,40 @@ function getHumanChoice() {
 }
 
 //console.log(getHumanChoice());
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice.toLowerCase();
+    if(humanChoice === computerChoice) {
+        return "It's a tie!";
+    }
+    if(humanChoice == "rock"){
+        if(computerChoice == "paper") {
+            computerScore++;
+            return "You lose! Paper beats Rock";
+        } else {
+            humanScore++;
+            return "You win! Rock beats Scissor";
+        }
+    }else if(humanChoice == "paper") {
+        if(computerChoice == "scissor") {
+            computerScore++;
+            return "You lose! Scissor beats Paper";
+        } else {
+            humanScore++;
+            return "You win! Paper beats Rock";
+        }
+    } else {
+        if(computerChoice == "rock") {
+            computerScore++;
+            return "You lose! Rock beats Scissor";
+        } else {
+            humanScore++;
+            return "You win! Scissor beats Paper";
+        }
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+console.log(playRound(humanSelection, computerSelection));
