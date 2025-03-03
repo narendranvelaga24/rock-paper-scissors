1a35b1819954f94ed1350677c9c5399ee8e3e16a
Narendran <175111990+Twenty625@users.noreply.github.com>
2025-03-03 13:41:17 +0530

    Remove the logic that plays exactly five rounds.

--- myscript.js
+++ myscript.js
@@ -1,10 +1,5 @@
 let humanScore = 0, computerScore = 0;
 
-/**
- * Generates a random choice for the computer in a rock-paper-scissors game.
- * 
- * @returns {string} - Returns one of the strings: "rock", "paper", or "scissor".
- */
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3);
     switch(choice) {
@@ -17,26 +12,11 @@ function getComputerChoice() {
     }
 }
 
-
-/**
- * Prompts the user to enter their choice of "rock", "paper", or "scissor".
- * Converts the user's input to lowercase to ensure consistency.
- * 
- * @returns {string} The user's choice in lowercase.
- */
 function getHumanChoice() {
     let choice = prompt("Please enter your choice: ", "rock/paper/scissor");
     return choice.toLowerCase(); 
 }
 
-
-/**
- * Plays a single round of Rock-Paper-Scissors and determines the winner.
- *
- * @param {string} humanChoice - The choice made by the human player. Expected values are "rock", "paper", or "scissor".
- * @param {string} computerChoice - The choice made by the computer player. Expected values are "rock", "paper", or "scissor".
- * @returns {string} - A message indicating the result of the round: whether it's a tie, or if the human player wins or loses.
- */
 function playRound(humanChoice, computerChoice) {
     console.log("Human: ", humanChoice);
     console.log("Computer: ", computerChoice);
@@ -70,18 +50,10 @@ function playRound(humanChoice, computerChoice) {
     }
 }
 
-
-/**
- * Initiates a game session where a human player competes against a computer.
- * The game consists of 5 rounds, and in each round, both the human and computer
- * make a selection. The result of each round is displayed to the player.
- */
 function playGame() {
-    for (let i = 0; i < 5; i++) {
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice(); 
-        alert(playRound(humanSelection, computerSelection)); 
-    }
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice(); 
+    alert(playRound(humanSelection, computerSelection));    
 }
 
 playGame();
